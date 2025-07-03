26acda0e78098397a49616ab949165f47bd84ae4
Jahidul Islam <jahidul.app@gmail.com>
2025-07-03 15:09:46 +0600

    styles: leaderboard card image 1

--- src/features/result/components/leaderboard-section.tsx
+++ src/features/result/components/leaderboard-section.tsx
@@ -59,8 +59,8 @@ const Card = ({
       <div className="flex justify-between items-center gap-3 ">
         {/* left */}
         <div className="flex items-center gap-3 w-full flex-1">
-          <div className="p-1 bg-accent rounded-full overflow-hidden">
-            <div className="relative w-10 aspect-square rounded-full overflow-hidden">
+          <div className="p-1 bg-accent w-10 aspect-square  rounded-full overflow-hidden">
+            <div className="relative w-full aspect-square rounded-full overflow-hidden">
               <Image
                 fill
                 src={`/api/image/${props?.image?.replace("/", "")}`}
