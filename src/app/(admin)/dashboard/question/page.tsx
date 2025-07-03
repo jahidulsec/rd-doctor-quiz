db1a19b152e1dd416615d62f7ba431b8c93fd92f
Jahidul Islam <jahidul.app@gmail.com>
2025-07-03 17:06:03 +0600

    fix: question table type

--- src/app/(admin)/dashboard/question/page.tsx
+++ src/app/(admin)/dashboard/question/page.tsx
@@ -37,7 +37,7 @@ const TableSection = async ({
 
   return (
     <ErrorBoundary error={response.error as any}>
-      <QuestionTable response={response} />
+      <QuestionTable response={response as any} />
     </ErrorBoundary>
   );
 };
