9c7eba0d95382fa8f0b4035bf0c532e61d8d28a4
Jahidul Islam <jahidul.app@gmail.com>
2025-07-14 14:45:15 +0600

    feat: adding register off page state

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,10 +1,21 @@
 import { LogoFull } from "@/components/logo/logo";
 import { Button } from "@/components/ui/button";
 import RegisterForm from "@/features/doctor/components/register-form";
+import { BadgeInfo } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
 export default function RegisterPage() {
+  const activeRegister = process.env.NEXT_PUBLIC_REGISTER_ACTIVE;
+
+  if (activeRegister === "0")
+    return (
+      <div className="flex flex-col justify-center items-center gap-5">
+        <BadgeInfo size={100} className="text-primary/50 fill-primary/40" />
+        <p className="text-muted-foreground">Registration has Ended!</p>
+      </div>
+    );
+
   return (
     <div className="my-6">
       <LogoFull className="text-primary mb-10" width={250} />
