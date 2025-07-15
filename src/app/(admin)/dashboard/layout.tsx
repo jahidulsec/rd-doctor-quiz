3d5d84f0299a2c5a8688f02ca52a7318e71a2e96
Jahidul Islam <jahidul.app@gmail.com>
2025-07-15 10:35:21 +0600

    styles: redesign app nav

--- src/app/(admin)/dashboard/layout.tsx
+++ src/app/(admin)/dashboard/layout.tsx
@@ -1,6 +1,11 @@
 import { AppSidebar } from "@/components/dashboard/app-sidebar";
-import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import NavRegister from "@/features/nav-bar/components/nav-register";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import Image from "next/image";
 import React from "react";
 
 export default function DashboardLayout({ children }: React.PropsWithChildren) {
@@ -8,9 +13,25 @@ export default function DashboardLayout({ children }: React.PropsWithChildren) {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset className="">
-        <div className="flex items-center px-4">
+        <div className="flex items-center border-b py-2 px-4 gap-5">
           <SidebarTrigger />
-          <NavRegister className="border-b-0 flex justify-between flex-wrap" />
+          <Separator orientation="vertical" className="max-h-5" />
+          <div className={"flex justify-between items-center gap-5 w-full"}>
+            <Image
+              src={"/images/Radiant Digital Health Logo.png"}
+              alt="Radiant Digital Health"
+              width={150}
+              height={100}
+            />
+
+            <div className="relative w-16 aspect-video">
+              <Image
+                src={"/images/Rivotril-1.svg"}
+                alt="Radiant Digital Health"
+                fill
+              />
+            </div>
+          </div>
         </div>
         <div className="px-4 p-2 flex flex-col gap-4 md:container md:mx-auto">
           {children}
