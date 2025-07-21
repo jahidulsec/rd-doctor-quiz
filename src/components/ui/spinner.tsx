73ed20e1a305588e7fd2e220734f8c39193594e1
Jahidul Islam <jahidul.app@gmail.com>
2025-07-21 18:25:33 +0600

    feat: update spinner

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils";
 import React from "react";
 
 function Spinner({
-  borderBottomColor = "primary",
+  borderBottomColor = "border-b-primary",
   className,
 }: {
   borderBottomColor?: string;
@@ -13,12 +13,12 @@ function Spinner({
       aria-label="Loading"
       className="relative inline-flex flex-col gap-2 items-center justify-center"
     >
-      <div className={cn(`relative flex w-[1rem] aspect-square`, className)}>
+      <div className={cn(`relative flex w-[2rem] aspect-square`, className)}>
         <span
-          className={`absolute w-full h-full rounded-full animate-spin duration-[800ms] ease-in-out border-t-transparent border-l-transparent border-r-transparent border-3 ${borderBottomColor}`}
+          className={`absolute w-full h-full rounded-full animate-[spin_1s_ease_infinite] border-t-transparent border-l-transparent border-r-transparent border-3 ${borderBottomColor}`}
         ></span>
         <span
-          className={`absolute w-full h-full rounded-full duration-[800ms] animate-spin ease-linear border-dotted border-t-transparent border-l-transparent border-r-transparent border-3 ${borderBottomColor}`}
+          className={`absolute w-full h-full rounded-full duration-[800ms] animate-[spin_1s_linear_infinite] border-dotted border-t-transparent border-l-transparent border-r-transparent border-3 ${borderBottomColor}`}
         ></span>
       </div>
     </div>
